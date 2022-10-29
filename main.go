48e34f9312c7417c93da434a11815a07794099ce
Mazen Mahmoud <mazen.mahmoud.freelancer@gmail.com>
2022-10-29 23:37:06 +0200

    Initialize app with simple route

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homepage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage Endpoint")
+}
+
+func handleRequests() {
+	http.HandleFunc("/main", homepage)
+	log.Fatal(http.ListenAndServe(":8001", nil))
+}
+
+func main() {
+	handleRequests()
+}

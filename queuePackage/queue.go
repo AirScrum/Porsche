a667be1dcbd62764a9b7159850e9c3d7ee9abdbe
Shehab Adel <shehabeldinadel@gmail.com>
2022-11-12 00:42:33 +0200

    Modified the logging inside to differentiate between userStoriesQueue and textQueue, and wrote the sendToUserStoriesQueue function, and declared the userStoriesQueue

--- queuePackage/queue.go
+++ queuePackage/queue.go
@@ -13,26 +13,50 @@ var ctx context.Context
 var textQueue amqp.Queue
 var textChannel *amqp.Channel
 
+var userStories amqp.Queue
+var userStoriesChannel *amqp.Channel
+
 func start() {
+	/*
+		Connection to RabbitMQ, TextChannel, and UserStoriesChannel
+	*/
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
-	textChannel, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
+	textChannel, textChannelError := conn.Channel()
+	failOnError(textChannelError, "[textQueue] - Failed to open text Channel")
 	defer textChannel.Close()
 
-	textQueue, err := textChannel.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
+	userStoriesChannel, userStoriesChannelError := conn.Channel()
+	failOnError(userStoriesChannelError, "[userStoriesQueue] - Failed to open userStories Channel")
+	defer userStoriesChannel.Close()
 
+	/*
+		Declaring textQueue, and userStoriesQueue
+	*/
+
+	textQueue, textChannelError := textChannel.QueueDeclare(
+		"textQueue", // name
+		false,       // durable
+		false,       // delete when unused
+		false,       // exclusive
+		false,       // no-wait
+		nil,         // arguments
+	)
+	failOnError(textChannelError, "[textQueue] - Failed to declare a text queue")
 	fmt.Println(textQueue)
+
+	userStoriesQueue, userStoriesChannelError := userStoriesChannel.QueueDeclare(
+		"userStoriesQueue", // name
+		false,              // durable
+		false,              // delete when unused
+		false,              // exclusive
+		false,              // no-wait
+		nil,                // arguments
+	)
+	failOnError(userStoriesChannelError, "[userStoriesQueue] - Failed to declare a userStories queue")
+	fmt.Println(userStoriesQueue)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
@@ -52,15 +76,26 @@ func sendToTextQueue(msg string) {
 			ContentType: "text/plain",
 			Body:        []byte(msg),
 		})
-	failOnError(err, "Failed to publish a message")
-	log.Printf(" [x] Sent %s\n", msg)
+	failOnError(err, "[textQueue] - Failed to publish a message to textQueue")
+	log.Printf("[textQueue] - [x] Sent %s\n", msg)
 
 }
 
 func sendToUserStoriesQueue(msg string) {
 	/*
-		TODO Implement  sendToUserStoriesQueue
+		TODO Implement  sendToUserStoriesQueue logic
 	*/
+	err := userStoriesChannel.PublishWithContext(ctx,
+		"",               // exchange
+		userStories.Name, // routing key
+		false,            // mandatory
+		false,            // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(msg),
+		})
+	failOnError(err, "[userStoriesQueue] - Failed to publish a message to userStoriesQueue")
+	log.Printf("[userStoriesQueue] - [x] Sent %s\n", msg)
 }
 func failOnError(err error, msg string) {
 

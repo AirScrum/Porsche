41aa56d2d1d969706e015dc5347ef395c9f5cd61
Shehab Adel <shehabeldinadel@gmail.com>
2022-11-14 00:23:45 +0200

    Fixed parsing Request body bug.

--- main.go
+++ main.go
@@ -1,14 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	queuepackage "goserver/queuePackage"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
+type Message struct {
+		Textid string `json:"textid"`
+		Text string `json:"text"`
+}
+type Bird struct {
+	Species string
+	Description string
+  }
 
 func homepage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Homepage Endpoint")
+	/*
+		Read the request body and parse it from JSON to Message Struct
+	*/
+	defer r.Body.Close()
+    buf, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+        panic(err)
+    }
+	fmt.Println(buf)
+	msg:= Message{} 
+	err = json.Unmarshal(buf,&msg)
+	fmt.Println(msg.Textid)
+	fmt.Println(msg.Text)
+	/*
+		TODO Read the textID from the database
+	*/
+	/*
+		TODO Send the text fetched from the database to the TextQueue
+	*/
 }
 
 func handleRequests() {

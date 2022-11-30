92515c50014fa58f74a5a7863f30b3fc5d074bdd
Mazen Mahmoud <mazen.mahmoud.freelancer@gmail.com>
2022-11-30 02:45:42 +0200

    Adding comments

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 package main
 
+/*
+Import important libraries
+*/
 import (
 	"encoding/json"
 	"fmt"
@@ -9,25 +12,34 @@ import (
 	"net/http"
 )
 
+/*
+Define the queues we need
+*/
 var userStoriesQueue *queuepackage.IQueue
 var textQueue *queuepackage.IQueue
 
+/*
+This function is called when the is a request in our server, which contains the textid and needed to be sent to the text queue
+*/
 func homepage(w http.ResponseWriter, r *http.Request) {
-	/*
-		Read the request body and parse it from JSON to Message Struct
-	*/
+
+	// Read the request body and parse it from JSON to Message Struct
 	defer r.Body.Close()
 	buf, err := ioutil.ReadAll(r.Body)
+
+	// Print error message when failing
 	if err != nil {
 		panic(err)
 	}
 
+	// Take the request body and put it in an object
 	request := queuepackage.Request{}
 	err = json.Unmarshal(buf, &request)
 	if err != nil {
 		panic(err)
 	}
 
+	// Define Message that will be sent to the text queue
 	msg := queuepackage.Message{}
 
 	// Get corresponding text from the TextID from database
@@ -35,9 +47,9 @@ func homepage(w http.ResponseWriter, r *http.Request) {
 	msg.Text = "Testing"
 	msg.UserID = request.UserID
 
-	//queuepackage.SendToTextQueue(queues, msg.TextID, msg.Text, msg.UserID)
-
+	// Send the message text queue
 	queuepackage.SendToQueue(textQueue, msg)
+
 	/*
 		TODO Read the textID from the database
 	*/
@@ -46,20 +58,28 @@ func homepage(w http.ResponseWriter, r *http.Request) {
 	*/
 }
 
+/*
+This function is called when a request is sent to our server, and sent the request to homepage function to handle it
+*/
 func handleRequests() {
 	fmt.Println("Server Started")
 	http.HandleFunc("/main", homepage)
 	log.Fatal(http.ListenAndServe(":8002", nil))
 }
 
+/*
+This is our main function
+*/
 func main() {
-	//queues = queuepackage.QueueInit()
-	//queuepackage.ReceiveFromUserStoriesQueue(queues)
 
+	// Define the needed queues
 	userStoriesQueue = queuepackage.QueueFactory("userStoriesQueue", "userStories")
 	textQueue = queuepackage.QueueFactory("textQueue", "text")
 
+	// Begin listeing to the user stories queue to sent its content back to the gateway
 	queuepackage.ReceiveFromQueueConc(userStoriesQueue)
+
+	// Handle any requests sent to our server
 	handleRequests()
 
 }
--- queuePackage/queue.go
+++ queuePackage/queue.go
@@ -1,5 +1,8 @@
 package queuepackage
 
+/*
+Import important libraries
+*/
 import (
 	"bytes"
 	"context"
@@ -11,22 +14,35 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+/*
+This struct is for sending to the text message queue, for the NLP model to take this text and convert it to user stories
+*/
 type Message struct {
 	TextID string `json:"textID"`
 	Text   string `json:"text"`
 	UserID string `json:"userID"`
 }
 
+/*
+This struct is used, when we have a request from the gateway with the text id, to get the corresponding text from database, then send it to text queue
+*/
 type Request struct {
 	TextID string `json:"textID"`
 	UserID string `json:"userID"`
 }
 
+/*
+This struct is used to get the user stories array from the user stories queue, that is received from the NLP model
+*/
 type UserStory struct {
 	UserStories []string `json:"userStories"`
 	TextID      string   `json:"textID"`
 	UserID      string   `json:"userID"`
 }
+
+/*
+This struct specifies all the needed attributes for managing queues
+*/
 type IQueue struct {
 	ctx       context.Context
 	ctxCancel context.CancelFunc
@@ -37,12 +53,16 @@ type IQueue struct {
 	queueType string
 }
 
+/*
+This function is for initializing the queues
+*/
 func QueueFactory(queueName string, queueType string) *IQueue {
 
 	myQueue := new(IQueue)
 	myQueue.name = queueName
 	myQueue.queueType = queueType
 
+	// To connect to the rabbitmq (message queue) server
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	//defer conn.Close()
@@ -63,18 +83,27 @@ func QueueFactory(queueName string, queueType string) *IQueue {
 		false,        // no-wait
 		nil,          // arguments
 	)
+
+	// Print error message when failing
 	failOnError(myQueue.err, "["+myQueue.name+"]"+" - Failed to declare a "+myQueue.queueType+" queue")
 	fmt.Println(myQueue.queue)
 
+	// Define queue context
 	myQueue.ctx, myQueue.ctxCancel = context.WithTimeout(context.Background(), 5*time.Second)
 	defer myQueue.ctxCancel()
 
+	// Printing the context of the queue
 	fmt.Println(myQueue.ctx)
 
 	return myQueue
 }
 
+/*
+This function is for sending in queue
+*/
 func SendToQueue(myQueue *IQueue, msg Message) {
+
+	// To encode the msg object into array of bytes to be sent in the queue
 	var buf bytes.Buffer
 	enc := gob.NewEncoder(&buf)
 
@@ -82,6 +111,7 @@ func SendToQueue(myQueue *IQueue, msg Message) {
 		log.Fatal(err)
 	}
 
+	// Send to the queue
 	err := myQueue.channel.PublishWithContext(myQueue.ctx,
 		"",                 // exchange
 		myQueue.queue.Name, // routing key
@@ -91,12 +121,19 @@ func SendToQueue(myQueue *IQueue, msg Message) {
 			ContentType: "text/plain",
 			Body:        buf.Bytes(),
 		})
+
+	// Print error message when failing
 	failOnError(err, "["+myQueue.name+"] - Failed to publish a message to the queue")
 
 	log.Printf("["+myQueue.name+"] - [x] Sent %s\n", msg.TextID)
 }
 
+/*
+This function is for receiving from the queue
+*/
 func ReceiveFromQueueConc(myQueue *IQueue) {
+
+	// Here we receive the message
 	msgs, err := myQueue.channel.Consume(
 		myQueue.queue.Name,
 		"",
@@ -107,13 +144,16 @@ func ReceiveFromQueueConc(myQueue *IQueue) {
 		nil,
 	)
 
+	// Print error message when failing
 	if err != nil {
 		panic(err)
 	}
 
+	// This function runs in the background and is used to always keep listenning to the queue for any incoing message
 	go func() {
 		for d := range msgs {
 			fmt.Printf("[%s] Received Message:\n %s\n\n", myQueue.name, d.Body)
+
 			//Decode the message and deserialize it
 			//to JSON format to be saved to the MongoDB
 
@@ -123,6 +163,7 @@ func ReceiveFromQueueConc(myQueue *IQueue) {
 	}()
 }
 
+// This function is used to print the error messages
 func failOnError(err error, msg string) {
 
 	if err != nil {

fc8e15f017336bd471e9b54ad16096c14cc35597
mazen-mahmoud16 <mazenmahmoudabdelhafeez@gmail.com>
2023-05-11 00:32:25 +0300

    Make queues durable

--- queuePackage/queue.go
+++ queuePackage/queue.go
@@ -53,7 +53,7 @@ func QueueFactory(queueName string, queueType string) *IQueue {
 
 	myQueue.queue, myQueue.err = myQueue.channel.QueueDeclare(
 		myQueue.name, // name
-		false,        // durable
+		true,         // durable
 		false,        // delete when unused
 		false,        // exclusive
 		false,        // no-wait
